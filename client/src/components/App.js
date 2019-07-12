1cc9b4e84caf6f09fdbc8be7baa21488c95ea20f
degalo93 <denisgalo10@gmail.com>
2019-07-11 19:05:47 -0700

    Starting with google authentication Oauth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route , Link  } from 'react-router-dom';
+import { BrowserRouter, Route  } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Navbar from './Navbar';
 
 
 
@@ -13,6 +14,7 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
+                <Navbar />
                 <Route path="/" exact component = {StreamList} />
                 <Route  path="/streams/new" exact component = {StreamCreate} />
                 <Route path="/streams/edit" exact component = {StreamEdit} />
---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    render () {
+        return <div>Google auth</div>
+    }
+}
+
+export default GoogleAuth;
