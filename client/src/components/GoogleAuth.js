4f31d779c21602e77758a6ae411c3cf31037b950
degalo93 <denisgalo10@gmail.com>
2019-07-11 22:48:04 -0700

    adding more on GoogleAuth code

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -4,7 +4,7 @@ class GoogleAuth extends React.Component {
 
     state = { isSignedIn: null };
 
-    //callback to make sure google recived the request 
+    //callback to make sure google recived the request includes my cliend id
     componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
@@ -12,10 +12,19 @@ class GoogleAuth extends React.Component {
                 scope: 'email'
             }).then(() => {
               this.auth = window.gapi.auth2.getAuthInstance(); 
-              this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+              this.onAuthChange ();
+              this.auth.isSignedIn.listen(this.onAuthChange);
             }); 
         });
     }
+
+//onAuthChange will display a change and show if user is signed in or not without prefreshing the page
+
+    onAuthChange = () => {
+        this.setState({isSignedIn: this.auth.isSignedIn.get() });
+
+    };
+
     renderAuthButton () {
         if (this.state.isSignedIn === null ){
             return <div> Don't know if I am signed in </div>
