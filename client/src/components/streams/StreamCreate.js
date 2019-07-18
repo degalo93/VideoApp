dba37669b68b752ddb31df6eec81c6ba0e713dd1
degalo93 <denisgalo10@gmail.com>
2019-07-17 17:26:58 -0700

    basic vaildation on create stream component

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput({input, label}) {
+    renderInput({input, label, meta}) {
+       
         return (
         <div className="field">
         <label>{label}</label>
         <input {...input}/>
+        <div>{meta.error}</div>
         </div>
         );
     }
 
-    onSubmit(formVaule) {
-       console.log (formVaule);
+    onSubmit(formVaules) {
+       console.log (formVaules);
     }
 
     render () { 
@@ -23,7 +25,19 @@ class StreamCreate extends React.Component {
                 <Field name="description" component= {this.renderInput} label="Enter Discription" />
                 <button className="ui button primary">Submit</button>
             </form>
-        )
-    }   
-}
-export default reduxForm ({ form : 'streamCreate' }) (StreamCreate);
+        );
+    }  
+};
+const validate = formValues => {
+    const errors = {};
+    if(!formValues.title){ 
+        errors.title = 'You must enter a title.';
+    }
+    if (!formValues.description){
+        errors.description = 'You must enter a description';
+    }
+    return errors;
+};
+
+
+export default reduxForm ({ form : 'streamCreate', validate }) (StreamCreate);
