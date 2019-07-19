adec4ba12a0bc532d1ae0ef2b2e8d50fe9807ed8
degalo93 <denisgalo10@gmail.com>
2019-07-18 21:18:23 -0700

    adding get and post requests in actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,12 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import { SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+ } from './types';
 
 
 export const signIn = userId => {
@@ -14,6 +21,7 @@ export const signOut = () => {
     };
 };
 
+// reminder to change dispatch later on
 export const createStream = formValues => async dispatch => {
   
    const response = await streams.post('/streams', formValues);
@@ -21,3 +29,13 @@ export const createStream = formValues => async dispatch => {
    dispatch ({ type: CREATE_STREAM, payload: response.data  });
 
 };
+
+export const fetchStreams = () => async dispatch => {
+  
+    const response = await streams.get('/streams');
+
+    dispatch ({ type: FETCH_STREAM, payload: response.data });
+
+ 
+ };
+ 
