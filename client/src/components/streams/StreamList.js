c39804af68489de924f0df9175d23408c54aa2e4
degalo93 <denisgalo10@gmail.com>
2019-07-22 00:27:00 -0700

    adding functionality to streamedit in component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -14,12 +14,8 @@ class StreamList extends React.Component {
         if(stream.userId === this.props.currentUserId){
             return (
             <div className="right floated content">
-                <button className="ui button primary">
-                    EDIT
-                </button>
-                <button className="ui button negative">
-                    DELETE
-                </button>
+               <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
+               <Link className="ui button negative">Delete</Link>
             </div>
             );
         }
