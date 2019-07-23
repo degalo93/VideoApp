276c41d84aff7766405abdffc0e4285a7ebc08e8
degalo93 <denisgalo10@gmail.com>
2019-07-22 20:20:38 -0700

    getting edit to function and change description and title

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream, editStream } from '../../actions';
@@ -10,7 +11,7 @@ class StreamEdit extends React.Component {
     }
 
     onSubmit = (formValues) => {
-
+        this.props.editStream(this.props.match.params.id, formValues);
     }
 
     render() {
@@ -21,7 +22,9 @@ class StreamEdit extends React.Component {
             
             <div>
                 <h3>Edit a Stream </h3>
-                <StreamForm initialValues={this.props.stream} onSubmit={this.onSubmit} />
+                <StreamForm 
+                initialValues={_.pick(this.props.stream, 'title' , 'description')} 
+                onSubmit={this.onSubmit} />
             </div>
 
         );
