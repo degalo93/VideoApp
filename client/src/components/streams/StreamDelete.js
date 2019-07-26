0608da95b2c9a349ed899a63b9be79aed18aa0bd
degalo93 <denisgalo10@gmail.com>
2019-07-26 11:50:59 -0700

    adding Stream title on delete

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Modal from '../Modal';
 import history from '../../history';
+import { fetchStream } from '../../actions';
 
 class StreamDelete extends React.Component {
 
-    componentDidMount(){
-        
+    componentDidMount() {
+       this.props.fetchStream(this.props.match.params.id);
     }
 
     renderActions() {
@@ -17,20 +19,29 @@ class StreamDelete extends React.Component {
         </React.Fragment>
         );
     }
+    renderContent(){
+        if (!this.props.stream){
+            return 'Are  you sure you want to delete this stream?'
+        }
+        return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`
+    }
     
    render(){
     return (
-        <div>
-            StreamDelete
+     
             <Modal 
                 title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
+                content={this.renderContent()}
                 actions={this.renderActions()}
                 onDismiss={() => history.push('/')}
             />
-        </div>
+        
     );
    }
 };
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, {fetchStream}) (StreamDelete);
