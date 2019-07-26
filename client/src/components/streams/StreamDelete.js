dd8f80040137ca944dba410ce5491ffdd9ee1a75
degalo93 <denisgalo10@gmail.com>
2019-07-26 11:08:11 -0700

    creating resusable modal with in app

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
 
     const actions = (
-        <div>
+        // you can just use open and close tags <> </> to position buttons properly
+        <React.Fragment>
             <button className="ui button negative">Delete</button>
             <button className="ui button ">Cancel</button>
-        </div>
+        </React.Fragment>
     );
     
     return (
@@ -17,6 +19,7 @@ const StreamDelete = () => {
                 title="Delete Stream"
                 content="Are you sure you want to delete this stream?"
                 actions={actions}
+                onDismiss={() => history.push('/')}
             />
         </div>
     );
